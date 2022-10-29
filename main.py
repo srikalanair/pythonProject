856c56b138aed92e0ac6a41b3010a5c98fdd1b99
srikala <srikalanair8@gmail.com>
2022-10-29 19:45:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# considering a list
+A = [1,2,3,4,5]
+#for adding an element
+    A. append an element[1,2,3,4,5]
+#for removing element
+    A.remove[1,2,3,4,5]
+#for sorting an list
+    A.sort[1,2,3,4,5]
+
+
